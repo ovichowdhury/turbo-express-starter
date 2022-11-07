c264489a77b29058d963f29aede57fd230b8928f
ovichowdhury <ovi.chowdhury@hotmail.com>
2022-11-07 10:53:52 +0600

    fix: docker and docker compose test and bug fix added

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+  output: 'standalone',
   experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
     transpilePackages: ['ui'],
   },
 }
